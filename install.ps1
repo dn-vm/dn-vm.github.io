b1aa6bc2a0514b36cd321956c18d9f1255a02eb8
agocke <agocke@users.noreply.github.com>
2023-01-20 00:02:36 +0000

    deploy: b17f8d919eaef2f9c5f8a2180e45881c7951be7b

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 
 $dnvmFeed = if ($env:DNVM_FEED) { $env:DNVM_FEED } else { "https://github.com/dn-vm/dnvm/releases/download" }
-$latestVersion = "0.3.2" # To be replaced during publish
+$latestVersion = "0.3.3" # To be replaced during publish
 
 function Get-Arch {
     $arch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
